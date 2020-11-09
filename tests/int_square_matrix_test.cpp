ed6f8a1e1a3af5feec39e3892cee01b3df597ab5
exterex <developer@sagen.io>
2020-11-09 13:41:12 +0100

    Added devcontainer and modified config

---
+++ tests/int_square_matrix_test.cpp
@@ -0,0 +1,11 @@
+#include "src/Matrix.h"
+
+int main(int argc, char** argv) {
+    test_square_initialization_and_methods();
+}
+
+void test_square_initialization_and_methods() {
+    // Init square matrix
+    Matrix<int> test_matrix_int(5);
+    
+}
