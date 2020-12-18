40b7324454d563bf65a2a4209846d98f331ff3e9
exTerEX <developer@sagen.io>
2020-12-18 00:14:06 +0000

    Added clang-format and styles

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include "include/libmatrix/matrix.h"
 
-int main(int argc, char **argv)
-{
-    matrix<int> m1(50, 10, -1);
-    m1.print();
+int main(int argc, char **argv) {
+  matrix<int> m1(50, 10, -1);
+  m1.print();
 }
