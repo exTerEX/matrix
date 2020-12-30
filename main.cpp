15f794cf80cbb32b375f7bc89afcee120319ba92
exTerEX <developer@sagen.io>
2020-12-30 17:23:42 +0000

    Current working test file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
-#include "include/libmatrix/matrix.h"
+#include "include/libmatrix/matrix.hpp"
+#include <iostream>
+#include <vector>
+
+std::vector<std::vector<int>> arr({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
 
 int main(int argc, char **argv) {
-  libmatrix::matrix<int> m1(50, 10, -1);
-  m1.print();
+  libmatrix::matrix<int> m({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+
+  m.print();
 }
