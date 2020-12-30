18c9f321f497e458e62c999ba3c418b102b53bec
exTerEX <developer@sagen.io>
2020-12-30 17:23:19 +0000

    Added a test, unfinished

---
+++ tests/operators/test_round_bracket.cpp
@@ -0,0 +1,18 @@
+#include "libmatrix/matrix.hpp"
+#include <cassert>
+
+// Test matrix
+libmatrix::matrix<int> m({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+
+// Test m(x, y)
+void round_bracket() {
+  int x = 1;
+  int y = 1;
+
+  int expected = 5;
+  int result = m(x, y);
+
+  assert(expected == result);
+}
+
+int main() { round_bracket(); }
