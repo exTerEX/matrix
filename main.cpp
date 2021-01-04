5de2215ee72087fdb787d1337d9768f6b76ab085
exTerEX <developer@sagen.io>
2021-01-04 21:10:14 +0000

    Reset main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,4 @@
 #include <iostream>
 #include <vector>
 
-std::vector<std::vector<int>> arr({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
-
-int main(int argc, char **argv) {
-  libmatrix::matrix<int> m({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
-
-  m.print();
-}
+int main(int argc, char **argv) {}
